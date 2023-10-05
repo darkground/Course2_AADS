ddd19dc7d391e3777387d69ac2466575d1402ce6
darkground <nailedhammer@yandex.ru>
2023-10-05 20:11:35 +0300

    rpn

--- aads1/aads1/aads1.cpp
+++ aads1/aads1/aads1.cpp
@@ -1,74 +1,95 @@
 ﻿#include <iostream>
-#include <sstream>
 #include <vector>
+#include <map>
 #include "list.h"
 #include "vector.h"
 #include "stack.h"
+#include <unordered_map>
 
 using namespace std;
 
-const string OPS_FUNC[] = {
-    "sin", "cos", "tan", "cot",
-    "sinh", "cosh", "tanh", "coth",
-};
-
-const string OPS_NONFUNC[] = {
-    "**", "//", "++", "--",
+const string OPS_ALL[] = {
+    "**", "//",
     "*", "/", "-", "+", "^",
-    "(", ")"
+    "(", ")",
+    "sin", "cos", "tan", "cot"
 };
 
+const string OPS_FUNC[] = {
+    "sin", "cos", "tan", "cot"
+};
+
+const map<string, int> OPS_WEIGHTS = { 
+    {"+", 1},
+    {"-", 1},
+    {"*", 2},
+    {"/", 2},
+    {"//", 2}, //same as /
+	{"^", 3},
+    {"**", 3}, //same as ^
+    {"(", 0},
+    {")", 0},
+    {"sin", 4},
+    {"cos", 4},
+    {"tan", 4},
+    {"cot", 4},
+};
+
+bool isOperation(const string& str) {
+	return find(begin(OPS_ALL), end(OPS_ALL), str) != end(OPS_ALL);
+}
+
+bool isFunc(const string& str) {
+	return find(begin(OPS_FUNC), end(OPS_FUNC), str) != end(OPS_FUNC);
+}
+
+bool isNumber(const string& str) {
+    string::const_iterator it = str.begin();
+    while (it != str.end() && isdigit(*it)) ++it;
+    return !str.empty() && it == str.end();
+}
+
+bool shouldMove(string token, Stack opstack) {
+	return OPS_WEIGHTS.find(opstack.front())->second >= OPS_WEIGHTS.find(token)->second;
+}
+
+bool contains(string s, string sub, unsigned at) {
+    for (unsigned i = at; i < s.size() && (i - at) < sub.size(); i++)
+        if (s[i] != sub[i - at])
+            return false;
+    return true;
+}
+
+string vecToString(vector<string> v) {
+    string result_s;
+    for (string& tok : v)
+        result_s += tok + ' ';
+    return result_s;
+}
 
 vector<string> tokenize(string text) {
     vector<string> tokens;
+    text.erase(std::remove(text.begin(), text.end(), ' '), text.end());
+
     string current = "";
     for (int i = 0; i < text.size(); i++) {
-        current.clear();
+        current.clear(); // Clear results received before.
         char c = text[i];
-        if (c == '-' && i + 1 < text.size() && isdigit(text[i + 1])) {
-            current += c;
-            c = text[++i];
-        }
+        // If character is a digit
         if (isdigit(c)) {
             current += c;
-            int j = i + 1;
-            for (; j < text.size(); j++) {
-                if (isdigit(text[j]) || text[j] == '.')
-                    current += text[j];
-                else break;
-            }
-            i = j - 1;
-            if (current.size() > 0) tokens.push_back(current);
+            // Iterate forwards and grab anything that is a digit or delimiter (.)
+            for (i++; i < text.size() && (isdigit(text[i]) || text[i] == '.'); i++) current += text[i];
+            tokens.push_back(current);
+            // Check if next symbol is '(' and add * if needed
+            if (i < text.size() && (isalpha(text[i]) || text[i] == '(')) tokens.push_back("*");
+            i--;
             continue;
         }
-        if (isalpha(c)) {
-            current += c;
-            int j = i + 1;
-            for (; j < text.size(); j++) {
-                if (isalpha(text[j])) current += text[j];
-                else break;
-            }
-            bool is_func = find(begin(OPS_FUNC), end(OPS_FUNC), current) != end(OPS_FUNC);
-            if (is_func) {
-                if (j < text.size() && text[j] == '(') {
-                    current += text[j++];
-                    tokens.push_back(current);
-                }
-            } else if (current.size() > 0) tokens.push_back(current);
-            i = j - 1;
-            continue;
-        }
-        for (auto& op : OPS_NONFUNC) {
-            bool match = true;
-            int m = 0;
-            for (; m < text.size() && m < op.size(); m++) {
-                if (text[i + m] != op[m]) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match) {
-                i += m - 1;
+        // Checking characters, if they match some operators
+        for (auto& op : OPS_ALL) {
+            if (contains(text, op, i)) {
+                i += op.size() - 1;
                 tokens.push_back(op);
                 break;
             }
@@ -77,17 +98,40 @@ vector<string> tokenize(string text) {
     return tokens;
 }
 
+vector<string> rpn(string& inpt_str) {
+    vector<string> result;
+    vector<string> str_tokens = tokenize(inpt_str);
+    Stack opStack;
+
+    for (string token : str_tokens) {
+        if (isNumber(token)) result.push_back(token);
+        else if (token == "(") opStack.push(token);
+        else if (token == ")") {
+            while (opStack.size() && opStack.front() != "(") result.push_back(opStack.pop());
+            if (opStack.size() == 0) throw "Unpaired bracket encountered";
+            opStack.pop();
+        } else if (isOperation(token)) {
+            while (opStack.size() && opStack.front() != "(" && shouldMove(token, opStack)) result.push_back(opStack.pop());
+            opStack.push(token);
+        }
+    }
+
+    while (opStack.size()) {
+        if (opStack.front() == "(") throw "Unpaired bracket encountered";
+        result.push_back(opStack.pop());
+    }
+
+    return result;
+}
+
 int main()
 {
-    cout << "example: a**b + 3sin(-10) + a(2*b ^ c - 2)" << endl;
-    auto x = tokenize("a**b + 3sin(-10) + a(2*b ^ c - 2)");
-    for (auto& v : x)
-        cout << "token: " << v << endl;
+    string ex = "5**4 + 3sin(12 + 1) + 2(2*4 ^ 7 - 2)";
+    cout << "example: " << ex << endl;
+    cout << "result: " << vecToString(rpn(ex)) << endl;
     while (true) {
         string i;
         getline(cin, i);
-        auto x = tokenize(i);
-        for (auto& v : x)
-            cout << "token: " << v << endl;
+        cout << "result: " << vecToString(rpn(i)) << endl;
     }
 }
