c8b42ec3231158b94a3624ba2424851b4bb07030
darkground <blockads32@gmail.com>
2023-12-09 17:39:10 +0300

    (t1/cw): input demo

--- T1_Coursework/T1_Coursework/T1_Coursework.cpp
+++ T1_Coursework/T1_Coursework/T1_Coursework.cpp
@@ -4,5 +4,21 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto edges = readGraph("input.txt");
+    DSU dsu(edges.size());
+
+    unsigned n = 0;
+    std::vector<Edge> mst;
+    for (Edge e : edges) {
+        if (dsu.find(e.u) != dsu.find(e.v)) {
+            n += e.weight;
+            mst.push_back(e);
+            dsu.unite(e.u, e.v);
+        }
+    }
+
+    for (Edge e : mst) {
+        std::cout << e.a << ' ' << e.b << std::endl;
+    }
+    std::cout << n;
 }
