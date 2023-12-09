6d89467bf64f07958b34647528cc2f2dc5086832
darkground <blockads32@gmail.com>
2023-12-09 18:23:00 +0300

    (t1/cw): graph class

--- T1_Coursework/T1_Coursework/graph.h
+++ T1_Coursework/T1_Coursework/graph.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include "dsu.h"
 
 // Edge Structure
 struct Edge {
@@ -28,12 +29,31 @@ struct Edge {
     }
 };
 
+class Graph {
+    std::vector<Edge> edges;
+    unsigned mass;
 
-// Read a graph from given file
-std::vector<Edge> readGraph(std::string path) {
+public:
+    Graph(std::vector<Edge>);
+
+    static Graph fromFile(std::string);
+    Graph mst();
+    std::vector<Edge> get_edges();
+    unsigned get_mass();
+};
+
+Graph::Graph(std::vector<Edge> e) {
+    edges = e;
+    mass = 0;
+    for (Edge edge : edges) {
+        mass += edge.weight;
+    }
+}
+
+Graph Graph::fromFile(std::string path) {
     std::vector<std::string> names;
     std::string next;
-    
+
     std::ifstream ifs(path);
     if (!ifs.is_open()) throw std::runtime_error("File not found");
 
@@ -45,6 +65,7 @@ std::vector<Edge> readGraph(std::string path) {
         names.push_back(next);
     }
 
+    unsigned m = 0;
     std::vector<Edge> edges;
     for (unsigned i = 0; i < names.size(); i++) {
         for (unsigned j = 0; j < names.size(); j++) {
@@ -53,11 +74,39 @@ std::vector<Edge> readGraph(std::string path) {
             if (i < j) {
                 if (w <= 0) throw std::runtime_error("Invalid weights in file");
                 Edge e(names, i, j, w);
+                m += w;
                 edges.push_back(e);
             }
         }
     }
 
     std::sort(edges.begin(), edges.end());
+
+    return Graph(edges);
+}
+
+Graph Graph::mst() {
+    DSU dsu(edges.size());
+
+    unsigned n = 0;
+    std::vector<Edge> mst;
+    for (Edge e : edges) {
+        if (dsu.find(e.u) != dsu.find(e.v)) {
+            n += e.weight;
+            mst.push_back(e);
+            dsu.unite(e.u, e.v);
+        }
+    }
+
+    return Graph(mst);
+}
+
+std::vector<Edge> Graph::get_edges()
+{
     return edges;
+}
+
+unsigned Graph::get_mass()
+{
+    return mass;
 }
